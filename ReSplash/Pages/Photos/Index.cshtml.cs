ccda58eec0bb8ff261e68593a228ecdaefe63046
Michael Trumbull <michael.trumbull@nscc.ca>
2023-10-03 21:46:48 -0300

    Add project files.

---
+++ ReSplash/Pages/Photos/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ReSplash.Data;
+using ReSplash.Models;
+
+namespace ReSplash.Pages.Photos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ReSplash.Data.ReSplashContext _context;
+        
+        public IList<Photo> Photos { get; set; } = default!;
+
+        public IndexModel(ReSplash.Data.ReSplashContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Photo != null)
+            {
+                Photos = await _context.Photo.ToListAsync();
+            }
+        }
+    }
+}
